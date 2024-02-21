af437e1390546e7951843ec829c35b4291f87414
rxjans <120579803+rxjans@users.noreply.github.com>
2024-02-21 14:49:55 +0530

    Connect to Database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    console.log("Connection Successful");
+})
+.catch((err) => console.log(err));
+
 const app = express();
 
 app.listen(3000, () => {
