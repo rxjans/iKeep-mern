599a8344e23c9888847a5be6847d0307cf4847ca
rxjans <120579803+rxjans@users.noreply.github.com>
2024-02-27 15:04:09 +0530

    complete sidebar of the dashboard

---
+++ client/src/components/DashProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DashProfile() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DashProfile
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
 import Header from '../components/header';
+import { useLocation } from 'react-router-dom';
+import DashProfile from '../components/DashProfile';
+import DashSidebar from '../components/DashSidebar';
+
 function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+
+  useEffect(()=> {
+    const params = new URLSearchParams(location.search);
+    const tabsFromUrl = params.get('tab');
+    if(tabsFromUrl){
+      setTab(tabsFromUrl);
+    }
+  }, [location.search])
+
+
   return (
+
     <>
     <Header />
-      <div className=' pt-20'>
-        Dashboard
+      {/* sidebar */}
+      <div>
+        <DashSidebar />
+      </div>
+      {/* profile */}
+      <div>
+        {
+          tab === 'profile' && <DashProfile />
+        }
       </div>
     </>
   )
