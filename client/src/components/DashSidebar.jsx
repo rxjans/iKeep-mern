2beff8049aebd232a6e8bda00df0dac6df38e744
rxjans <120579803+rxjans@users.noreply.github.com>
2024-03-03 12:35:53 +0530

    add admin functionality to the user

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -53,7 +53,7 @@ function DashSidebar() {
                         <span class="mx-4 font-medium">Profile</span>
                     </button>
 
-                    <button onClick={() => handleButtonClick('accounts')} class={`flex lg:w-[220px] w-full items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-600/20 dark:hover:text-gray-200 hover:text-gray-700 ${activeButton === 'dashboard' && 'dark:bg-gray-600/20 text-white bg-gray-100'}`} >
+                    {/* <button onClick={() => handleButtonClick('accounts')} class={`flex lg:w-[220px] w-full items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-600/20 dark:hover:text-gray-200 hover:text-gray-700 ${activeButton === 'dashboard' && 'dark:bg-gray-600/20 text-white bg-gray-100'}`} >
                         <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                             <path d="M12 14C8.13401 14 5 17.134 5 21H19C19 17.134 15.866 14 12 14Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -68,10 +68,10 @@ function DashSidebar() {
                         </svg>
 
                         <span class="mx-4 font-medium">Tickets</span>
-                    </button>
+                    </button> */}
 
-                    <button onClick={handleSignout} class={`flex lg:w-[220px] w-full items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-600/20 dark:hover:text-gray-200 hover:text-gray-700 ${activeButton === 'signout' && 'dark:bg-gray-600/20 dark:text-white bg-gray-100'}`}>
-                        <IoMdLogOut />
+                    <button onClick={handleSignout} class={`flex lg:w-[220px] w-full items-center px-[10px] py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-600/20 dark:hover:text-gray-200 hover:text-gray-700 ${activeButton === 'signout' && 'dark:bg-gray-600/20 dark:text-white bg-gray-100'}`}>
+                        <IoMdLogOut className='w-[30px] h-[22px] ' />
 
                         <span class="mx-4 font-medium">Sign Out</span>
                     </button>
