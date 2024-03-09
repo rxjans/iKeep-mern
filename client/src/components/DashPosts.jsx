9e84fc097335a25c911630730d931cf8048939ee
rxjans <120579803+rxjans@users.noreply.github.com>
2024-03-09 22:48:05 +0530

    complete delete post functionality and delete post ui

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -7,6 +7,9 @@ function DashPosts() {
   const {currentUser} = useSelector((state)=>state.user);
   const [userPosts, setUserPosts]= useState([]);
   const [showMore, setShowMore] = useState(true);
+  const [totalPosts, setTotalPosts] = useState(null);
+  const [showModals, setShowModals] = useState(false);
+  const [postId, setPostId] = useState(null);
   const navigate = useNavigate();
   useEffect(()=> {
     const fetchPosts = async()=>{
@@ -15,9 +18,10 @@ function DashPosts() {
         const data = await res.json();
         if(res.ok){
           setUserPosts(data.posts);
-          if(data.posts.length < 9){
+          if(data.totalPosts <= 9){
             setShowMore(false);
           }
+          console.log(data.posts.length);
         }
 
       } catch (error) {
@@ -36,9 +40,31 @@ function DashPosts() {
       const data = await res.json();
       if(res.ok){
         setUserPosts([...userPosts, ...data.posts]);
-        if(data.posts.length < 9){
-          setShowMore(false);
-        }
+        setTotalPosts(data.totalPosts);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    if(userPosts.length == totalPosts){
+      setShowMore(false);
+    }
+  },[userPosts])
+  
+  const handleDelete = async()=>{
+    setShowModals(false);
+    try {
+      const res = await fetch(`/api/post/deletepost/${currentUser._id}/${postId}`,{
+        method: 'DELETE'
+      })
+      const data = await res.json();
+      if(res.ok){
+        setUserPosts(userPosts.filter((post)=>post._id !== postId))
+      }
+      if(!res.ok){
+        console.log(data.message);
       }
     } catch (error) {
       console.log(error);
@@ -96,7 +122,7 @@ function DashPosts() {
                                 {currElem.category}
                             </td>
                             <td class="px-6 py-4">
-                                <a href="#" class="font-medium text-red-500 dark:text-red-500 hover:underline">Delete</a>
+                                <a onClick={(e)=>{e.preventDefault(); setShowModals(true); setPostId(currElem._id);}} href="#" class="font-medium text-red-500 dark:text-red-500 hover:underline">Delete</a>
                             </td>
                             <td class="px-6 py-4">
                               <Link to={`/update-post/${currElem._id}`}>
@@ -122,6 +148,40 @@ function DashPosts() {
         </>) 
         : 
         (<p>You have no posts yet</p>)
+      }
+      {
+      showModals &&
+      <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true" >
+              
+                <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+                  <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                      
+                      <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                        <div className="bg-white dark:bg-[rgb(35,39,42)] px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                          <div className="sm:flex sm:items-start">
+                            <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 dark:bg-red-200 sm:mx-0 sm:h-10 sm:w-10">
+                              <svg className="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                              </svg>
+                            </div>
+                            <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                              <h3 className="text-base font-semibold leading-6 text-gray-900 dark:text-gray-200" id="modal-title">Delete post</h3>
+                              <div className="mt-2">
+                                <p className="text-sm dark:text-gray-400 text-gray-500">Are you sure you want to delete this post? This action cannot be undone.</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="dark:bg-[rgb(35,39,42)] px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                          <button onClick={handleDelete} type="button" className="inline-flex w-full justify-center rounded-md bg-red-600 dark:bg-red-800/90 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 dark:hover:bg-red-600/50 sm:ml-3 sm:w-auto">Delete</button>
+                          <button onClick={()=> setShowModals(false)} type="button" className="mt-3 inline-flex w-full justify-center rounded-md bg-white dark:bg-gray-400 dark:text-gray-900 hover:dark:bg-gray-500 hover:bg-gray-200 px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300  sm:mt-0 sm:w-auto">Cancel</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
       }
     </div>
             
