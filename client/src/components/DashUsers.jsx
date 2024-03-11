45391c955f41c5dfb23f26583edc930e6fa986f0
rxjans <120579803+rxjans@users.noreply.github.com>
2024-03-11 21:47:18 +0530

    add delete popup to users

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -9,6 +9,8 @@ function DashUsers() {
     const [totalUsers, setTotalUsers]= useState(null);
     const {currentUser} = useSelector((state)=> state.user);
     const [showMore, setShowMore] = useState(true);
+    const [showModals, setShowModals] = useState(false);
+    const [userId, setUserId] = useState(null);
     useEffect(()=>{
         const fetchUsers = async()=>{
             try {
@@ -51,6 +53,10 @@ function DashUsers() {
         } catch (error) {
             console.log(error);
         }
+    }
+
+    const handleDelete = async()=>{
+
     }
 
   return (
@@ -112,7 +118,7 @@ function DashUsers() {
                                 }
                             </td>
                             <td class="px-6 py-4">
-                                <a /*onClick={(e)=>{e.preventDefault(); setShowModals(true); setPostId(currElem._id);}}*/ href="#" class="font-medium text-red-500 dark:text-red-500 hover:underline">Delete</a>
+                                <a onClick={(e)=>{e.preventDefault(); setShowModals(true); setUserId(currElem._id);}} href="#" class="font-medium text-red-500 dark:text-red-500 hover:underline">Delete</a>
                             </td>
                         </tr>
                     )
@@ -133,6 +139,40 @@ function DashUsers() {
         </>) 
         : 
         (<p>There are no users yet</p>)
+      }
+      {
+      showModals &&
+      <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true" >
+              
+                <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+                  <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                      
+                      <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                        <div className="bg-white dark:bg-[rgb(35,39,42)] px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                          <div className="sm:flex sm:items-start">
+                            <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 dark:bg-red-200 sm:mx-0 sm:h-10 sm:w-10">
+                              <svg className="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                              </svg>
+                            </div>
+                            <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                              <h3 className="text-base font-semibold leading-6 text-gray-900 dark:text-gray-200" id="modal-title">Delete User</h3>
+                              <div className="mt-2">
+                                <p className="text-sm dark:text-gray-400 text-gray-500">Are you sure you want to delete this user? This action cannot be undone.</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="dark:bg-[rgb(35,39,42)] px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                          <button onClick={handleDelete} type="button" className="inline-flex w-full justify-center rounded-md bg-red-600 dark:bg-red-800/90 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 dark:hover:bg-red-600/50 sm:ml-3 sm:w-auto">Delete</button>
+                          <button onClick={()=> setShowModals(false)} type="button" className="mt-3 inline-flex w-full justify-center rounded-md bg-white dark:bg-gray-400 dark:text-gray-900 hover:dark:bg-gray-500 hover:bg-gray-200 px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300  sm:mt-0 sm:w-auto">Cancel</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
       }
     </div>
   )
