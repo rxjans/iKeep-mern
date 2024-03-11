1bd6e8fe8c0d2f0bcc443dc8c50f0ee1c36220f2
rxjans <120579803+rxjans@users.noreply.github.com>
2024-03-11 18:10:22 +0530

    create getUsers api route

--- api/Controllers/userController.js
+++ api/Controllers/userController.js
@@ -68,3 +68,38 @@ export const signout = async(req, res, next)=> {
         next(error);
     }
 }
+
+export const getusers= async(req, res, next)=>{
+    if(!req.user.isAdmin){
+        return next(errorHandler(403, "you are not allowed to view the users"));
+    }
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = (req.query.sortDirection === 'asc' ? 1 : -1); 
+        const users = await User.find().sort({updatedAt: sortDirection}).skip(startIndex).limit(limit);
+
+        const usersNoPassword= users.map((user)=>{
+            const {password, ...rest} = user._doc;
+            return rest;
+        })
+
+        const totalUsers = await User.countDocuments();
+
+        const now = new Date();
+
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() -1,
+            now.getDate()
+        );
+
+        const lastMonthUsers = await User.countDocuments({
+            createdAt: {$gte: oneMonthAgo}
+        });
+
+        res.status(200).json({usersNoPassword, totalUsers, lastMonthUsers});
+    } catch (error) {
+        next(error);
+    }
+}
