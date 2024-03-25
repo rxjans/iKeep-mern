dabca03e4cf430b54e6929233d49e6013e47a75a
rxjans <120579803+rxjans@users.noreply.github.com>
2024-03-25 19:21:27 +0530

    create like comment api route

--- api/Controllers/commentController.js
+++ api/Controllers/commentController.js
@@ -1,4 +1,5 @@
 import Comment from '../models/commentModel.js';
+import { errorHandler } from '../utils/errorHandler.js';
 
 export const createComment = async(req,res, next)=>{
     try {
@@ -25,5 +26,27 @@ export const getComments = async(req, res, next)=>{
         res.status(200).json(comments);
     } catch (error) {
         next(error)
+    }
+}
+
+export const likeComment = async(req, res, next) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId);
+        if(!comment){
+            next(errorHandler(404, "Comment not found..."))
+        }
+        const userIndex = comment.likes.indexOf(req.user.id);
+        if(userIndex === -1){
+            comment.numberOfLikes += 1;
+            comment.likes.push(req.user.id);
+        }
+        else{
+            comment.numberOfLikes -= 1;
+            comment.likes.splice(userIndex, 1);
+        }
+        await comment.save();
+        res.status(200).json(comment);
+    } catch (error) {
+        next(error);
     }
 }
--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createComment, getComments } from "../Controllers/commentController.js";
+import { createComment, getComments, likeComment } from "../Controllers/commentController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
 router.get('/getcomments/:postId', getComments);
+router.put('/likecomment/:commentId', verifyToken, likeComment);
 
 export default router;
