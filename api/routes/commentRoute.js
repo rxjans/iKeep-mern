41d7316bea444a0218a336557abcb011f5662d83
rxjans <120579803+rxjans@users.noreply.github.com>
2024-03-29 17:10:35 +0530

    create comment delete api route

--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createComment, editComment, getComments, likeComment } from "../Controllers/commentController.js";
+import { createComment, deleteComment, editComment, getComments, likeComment } from "../Controllers/commentController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.post("/create", verifyToken, createComment);
 router.get('/getcomments/:postId', getComments);
 router.put('/likecomment/:commentId', verifyToken, likeComment);
 router.put('/editcomment/:commentId', verifyToken, editComment);
+router.delete('/deletecomment/:commentId', verifyToken, deleteComment);
 
 
 export default router;
